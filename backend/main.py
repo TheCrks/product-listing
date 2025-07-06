cdb084337674a3553eb7c42c2b4838477859ba74
TheCrks <anadoluyuz_46@hotmail.com>
2025-07-06 14:19:19 +0300

    fix: added try catch to mounting for better runtime mangement

--- backend/main.py
+++ backend/main.py
@@ -23,9 +23,12 @@ app = configureCors(app)
 # define routes
 app.include_router(api_fetcher_router,prefix=Config.settings.API_V1_STR)
 
-dist_path = os.path.join(BASE_DIR, "..", "frontend", "product-listing", "product-listing", "dist")
-# Serve frontend from /frontend/dist
-app.mount("/", StaticFiles(directory=dist_path, html=True), name="static")
+try:
+      dist_path = os.path.join(BASE_DIR, "..", "frontend", "product-listing", "product-listing", "dist")
+      # Serve frontend from /frontend/dist
+      app.mount("/", StaticFiles(directory=dist_path, html=True), name="static")
+except Exception as e:
+      print("Mounting the frontend directory failed: " + str(e))
 
 if __name__ == "__main__":
       uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
